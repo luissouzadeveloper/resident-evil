d9818ba627c6f80dda5141bebdbdcbdc25bb022b
Luis Felipe <luisfelipeosouzati@gmail.com>
2024-02-16 20:24:50 -0300

    Ajustes do menu e implementação dos botões de seleção

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 // Menu mobile
-let botao = document.querySelector('.abrir');
-let menu = document.querySelector('.menu-mobile');
+function funcionalidadeMenu() {
+    let botao = document.querySelector('.abrir');
+    let menu = document.querySelector('.menu-mobile');
 
-botao.addEventListener('click', function(){
-    if(menu.classList.contains('abrir-menu')) {
-        menu.classList.remove('abrir-menu');
-    } else {
-        menu.classList.add('abrir-menu');
-    }
-});
+    botao.addEventListener('click', function () {
+        if (menu.classList.contains('abrir-menu')) {
+            menu.classList.remove('abrir-menu');
+        } else {
+            menu.classList.add('abrir-menu');
+        }
+    });
+}
+
+funcionalidadeMenu();
